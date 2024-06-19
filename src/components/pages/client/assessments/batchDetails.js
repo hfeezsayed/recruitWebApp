83a255091d5c58776f175536399adff84261c605
operations <operation@aviskaran.com>
2024-06-19 12:14:46 +0530

    fixing issue

--- src/components/pages/client/assessments/batchDetails.js
+++ src/components/pages/client/assessments/batchDetails.js
@@ -54,10 +54,10 @@ export const AssesmentBatchDetails = () => {
 
   useEffect( () => {
     const user = JSON.parse(localStorage.getItem("token"));
-    axios.get("http://localhost:8080/xen/getAssessments?clientId="+user.userId+"&pageNo=1&pageSize=12")
+    axios.get("http://localhost:8080/xen/getAssessments?clientId="+user.userId+"&pageNo=1&pageSize=5")
     .then(response => {
         console.log(response.data.data);
-        setData(response.data.data);
+        //setData(response.data.data);
     })
     .catch(error => {
       console.log(error);
